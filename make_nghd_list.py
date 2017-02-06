521a849a6fc59619ffe8ed39b8b04ccb89dc921f
Dan Tasse <dan.tasse@gmail.com>
2017-02-06 14:59:11 -0800

    neighborhood list file

---
+++ make_nghd_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Just make a file that looks like this:
+# { "Pittsburgh": ["Shadyside", "Squirrel Hill North", ...],
+#   "Chicago": ["Wicker Park", "Logan Square", ...] ...}
+
+import argparse, csv, collections, json
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--output_file', default='nghd_list.json')
+parser.add_argument('--source_files', nargs='+', default= ['nghd_bounds/austin.geojson',
+        'nghd_bounds/chicago.geojson', 'nghd_bounds/houston.geojson',
+        'nghd_bounds/pgh.geojson', 'nghd_bounds/sf.geojson'],
+    help='All the neighborhood bounds files')
+args = parser.parse_args()
+
+CITY_NAME_MAPPING = {'austin': 'Austin', 'chicago': 'Chicago',
+        'houston': 'Houston', 'pgh': 'Pittsburgh', 'sf': 'San Francisco'}
+
+output = {}
+for file in args.source_files:
+    city_name = CITY_NAME_MAPPING[file.replace('nghd_bounds/', '').replace('.geojson', '')]
+    json_data = json.load(open(file))
+    nghd_list = [n['properties']['name'] for n in json_data['features']]
+    output[city_name] = sorted(nghd_list)
+json.dump(output, open(args.output_file, 'w'))
+
